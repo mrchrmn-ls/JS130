ed3ee5fe10e184d72035b42f196fc11cb2db4828
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2020-09-17 11:50:55 +0200

    first commit

---
+++ 01-Iteration/filter-reduce.js
@@ -0,0 +1,19 @@
+function filter(array, callback) {
+  return array.reduce((filtered, element) => {
+    if (callback(element)) {
+      filtered.push(element);
+    }
+    return filtered;
+  }, [])
+}
+
+
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(filter(numbers, number => number > 3)); // => [ 4, 5 ]
+console.log(filter(numbers, number => number < 0)); // => []
+console.log(filter(numbers, () => true));           // => [ 1, 2, 3, 4, 5 ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(filter(values, value => typeof value === "string"));
+// => [ 'abc', 'xyz' ]
