58f6edfad39e6a3f026fc21ffc16bac765954569
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-03-14 11:58:03 +0100

    review

--- Exercises/01/06-anonymizer.js
+++ Exercises/01/06-anonymizer.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let Account = (function() {
+function makeAccount() {
   let userEmail, userPassword, userFirstName, userLastName;
 
   function isValidPassword(password) {
@@ -60,10 +60,10 @@ let Account = (function() {
     }
   };
 
-})();
-
-
-let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
+}
+let otherBar = Object.create(makeAccount()).init('other@bar.com', '232323', 'other', 'bar');
+let fooBar = Object.create(makeAccount()).init('foo@bar.com', '123456', 'foo', 'bar');
+console.log(otherBar.firstName('232323'));                     // returns the firstName function
 console.log(fooBar.firstName);                     // returns the firstName function
 console.log(fooBar.email);                         // returns the email function
 console.log(fooBar.firstName('123456'));           // logs 'foo'
@@ -73,6 +73,5 @@ console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password';
 console.log(fooBar.resetPassword('123456', 'abc')) // logs true
 
 let displayName = fooBar.displayName;
-console.log(displayName);
 fooBar.reanonymize('abc');                         // returns true
 console.log(displayName === fooBar.displayName);   // logs false
